112ac4da195617d020c777fb8228dd70d8f1eb52
박세환/메세징플랫폼개발팀/NE <gehwan96@nhn.com>
2023-05-06 19:15:22 +0900

    refactoring first - my opinion
    첫번째 리팩토링- 내 의견

--- refactoring/src/myopinion/Customer.java
+++ refactoring/src/myopinion/Customer.java
@@ -25,26 +25,9 @@ public class Customer {
         Enumeration rentals = _rentals.elements();
         String result = getName() + " 고객님의 대여 기록\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
-            Rental each = (Rental) rentals.nextElement();
 
-            switch(each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if(each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
+            Rental each = (Rental) rentals.nextElement();
+            double thisAmount = each.getCharge();
             frequentRenterPoints++;
 
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
