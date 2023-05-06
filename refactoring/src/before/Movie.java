491978859150b7e59ce740574b0d54dc04c39818
박세환/메세징플랫폼개발팀/NE <gehwan96@nhn.com>
2023-05-06 18:37:34 +0900

    init refactoring example
    refactoring 예시 초기 클래스 작성

---
+++ refactoring/src/before/Movie.java
@@ -0,0 +1,30 @@
+package before;
+
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+    public String _title;
+    public int _priceCode;
+
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public int getPriceCode() {
+        return _priceCode;
+    }
+
+    public void setTitle(String arg) {
+        this._title = arg;
+    }
+
+    public void setPriceCode(int arg) {
+        this._priceCode = arg;
+    }
+}
